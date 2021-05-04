bfe428cc57cca8da3685c6408755d62f7e3ccc06
Saif Ali Shaik <saifshine7@gmail.com>
2021-05-04 17:16:00 +0530

    👌 IMPROVE: octo cat picture

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,16 @@ async function renderApp() {
   }
 }
 
+function renderSidebar() {
+  const pick = document.querySelector.bind(document);
+
+  let createIssBtn = pick('.create-issue');
+  createIssBtn.addEventListener('fwClick', createIssue);
+
+  async function createTicket() { }
+
+}
+
 async function showNotification(status, message) {
   const details = {
     type: `${status}`,
@@ -23,41 +33,5 @@ async function showNotification(status, message) {
     await client.interface.trigger('showNotify', details);
   } catch (error) {
     console.error(error);
-  }
-}
-
-function renderSidebar() {
-  const pick = document.querySelector.bind(document);
-  const [subBox, descBox, emailBox] = [pick('.subject'), pick('.description'), pick('.email')];
-
-  let createTktBtn = document.querySelector('.create-ticket');
-  createTktBtn.addEventListener('fwClick', createTicket);
-
-  async function createTicket() {
-    const endpoint = `https://<%=iparam.subdomain%>.freshservice.com/api/v2/tickets`;
-    let body = {
-      description: `${descBox.value}`,
-      email: `${emailBox.value}`,
-      subject: `${subBox.value}`,
-      priority: 1,
-      status: 2
-    };
-
-    body = JSON.stringify(body);
-    let options = {
-      headers: {
-        Authorization: 'Basic <%= encode(iparam.freshservice_api_key) %>',
-        'Content-Type': 'application/json'
-      },
-      body
-    };
-    console.log(body);
-    try {
-      await client.request.post(endpoint, options);
-      await showNotification('success', 'The ticket is created');
-    } catch (error) {
-      console.log('error', error);
-      await showNotification('error', 'Something went wrong while creating ticket');
-    }
   }
 }
