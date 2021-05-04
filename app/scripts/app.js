a8d634652d07295b5215ba6148f1118d3acdfeb8
Saif Ali Shaik <saifshine7@gmail.com>
2021-05-04 08:20:07 +0530

    📦 NEW: serverles part

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@ document.onreadystatechange = function () {
 
 async function renderApp() {
   try {
-    let client = await app.initialized();
+    let _client = await app.initialized();
     window['client'] = _client;
     client.events.on('app.activated', renderSidebar);
     return;
@@ -19,27 +19,45 @@ async function showNotification(status, message) {
     type: `${status}`,
     message: `${message}`
   };
-  await client.interface.trigger('showNotify', details);
+  try {
+    await client.interface.trigger('showNotify', details);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function renderSidebar() {
-  const [subBox, descBox, emailBox] = [
-    document.querySelector('.subject'),
-    document.querySelector('.description'),
-    document.querySelector('.email')
-  ];
+  const pick = document.querySelector.bind(document);
+  const [subBox, descBox, emailBox] = [pick('.subject'), pick('.description'), pick('.email')];
+
   let createTktBtn = document.querySelector('.create-ticket');
   createTktBtn.addEventListener('fwClick', createTicket);
 
   async function createTicket() {
-    const endpoint = `https://<%=iparam.freshdesk_subdomain%>.freshdesk.com/api/v2/tickets`;
-    const options = {
-      headers: {
-        Authorization: ''
-      }
+    const endpoint = `https://<%=iparam.subdomain%>.freshservice.com/api/v2/tickets`;
+    let body = {
+      description: `${descBox.value}`,
+      email: `${emailBox.value}`,
+      subject: `${subBox.value}`,
+      priority: 1,
+      status: 2
     };
-    await client.request.post(endpoint, options);
-  }
 
-  console.log(subBox, descBox, emailBox, createTktBtn);
+    body = JSON.stringify(body);
+    let options = {
+      headers: {
+        Authorization: 'Basic <%= encode(iparam.freshservice_api_key) %>',
+        'Content-Type': 'application/json'
+      },
+      body
+    };
+    console.log(body);
+    try {
+      await client.request.post(endpoint, options);
+      await showNotification('success', 'The ticket is created');
+    } catch (error) {
+      console.log('error', error);
+      await showNotification('error', 'Something went wrong while creating ticket');
+    }
+  }
 }
